3d8d4253a92ab583e9676c904e311dfd92d21a38
fosser2 <andrew@ubuntu.(none)>
2013-02-23 22:21:46 -0600

    smba1002: removed avp hack; is now fixed with the new panel driver https://github.com/fosser2/android-tegra-nv-3.1.10-rel-15r7/commit/ee6eade92b3376ad2ecee16b58e2eecd49f92054

--- device-smba1002.mk
+++ device-smba1002.mk
@@ -54,8 +54,6 @@ PRODUCT_COPY_FILES += \
 	device/malata/smba1002/prebuilt/vold.fstab:system/etc/vold.fstab \
 	device/malata/smba1002/prebuilt/ramdisk/fstab.harmony:root/fstab.harmony \
 	device/malata/smba1002/prebuilt/03sysctl:system/etc/init.d/03sysctl \
-	device/malata/smba1002/prebuilt/test.mp4:system/etc/test.mp4 \
-	device/malata/smba1002/prebuilt/99avp_hack:system/etc/init.d/99avp_hack \
 	device/malata/smba1002/prebuilt/ntfs-3g:system/xbin/ntfs-3g
 
 #Set default.prop properties for root + adb
